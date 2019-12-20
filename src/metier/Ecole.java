420358dd6fc942a53395653a15dcc014c5e2e8a3
lo <ahmadou19@gmail.com>
2019-12-20 14:43:16 +0100

    ajout classe

--- src/metier/Ecole.java
+++ src/metier/Ecole.java
@@ -1,5 +1,49 @@
 package metier;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Ecole {
 
+	
+	private String nomEcole;
+	private String adresseEcole;
+	private List<Etudiant> etudiants = new ArrayList<Etudiant>();
+	
+	
+	public Ecole(String nomEcole, String adresseEcole) {
+		super();
+		this.nomEcole = nomEcole;
+		this.adresseEcole = adresseEcole;
+	}
+
+	public String getNomEcole() {
+		return nomEcole;
+	}
+
+	public void setNomEcole(String nomEcole) {
+		this.nomEcole = nomEcole;
+	}
+
+	public String getAdresseEcole() {
+		return adresseEcole;
+	}
+
+	public void setAdresseEcole(String adresseEcole) {
+		this.adresseEcole = adresseEcole;
+	}
+
+	public List<Etudiant> getEtudiants() {
+		return etudiants;
+	}
+
+	public void setEtudiants(List<Etudiant> etudiants) {
+		this.etudiants = etudiants;
+	}
+
+	@Override
+	public String toString() {
+		return "Ecole [nomEcole=" + nomEcole + ", adresseEcole=" + adresseEcole + "]";
+	}
+		
 }
