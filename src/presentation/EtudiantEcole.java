5eda5f02664f83bb1ebb958ff94f57b83a076690
lo <ahmadou19@gmail.com>
2019-12-20 16:14:19 +0100

    lecture

--- src/presentation/EtudiantEcole.java
+++ src/presentation/EtudiantEcole.java
@@ -2,6 +2,7 @@ package presentation;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Scanner;
@@ -14,7 +15,7 @@ public class EtudiantEcole {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner scan = new Scanner(System.in);
+		/*Scanner scan = new Scanner(System.in);
 		System.out.print("entrer le nom de l'etudiant : ");
 		String nom = scan.next();
 		System.out.println();
@@ -22,7 +23,9 @@ public class EtudiantEcole {
 		String prenom = scan.next();
 		Etudiant etudiant = new Etudiant( nom, prenom);
 		creerEtudiant(etudiant);
+		*/
 		
+		lireEtudiant();
 
 	}
 	
@@ -76,6 +79,61 @@ public static void creerEtudiant(Etudiant etudiant) {
 		}
 	}
 	
+public static void lireEtudiant() {
 	
+	// information de la base de donnee
+	
+			String url = "jdbc:mysql://localhost/etudiantecole";
+			String login = "root";
+			String password = "Passer@18";
+			Connection cn = null;
+			Statement st = null;
+			ResultSet rs = null;
+			
+			try {
+				// etape1 chargement du driver
+				
+				Class.forName("com.mysql.jdbc.Driver");
+				
+				// etape2 recupertion de la connnexion
+				
+				cn = DriverManager.getConnection(url, login, password);
+				
+				// etape 3 creation du statement
+				st = cn.createStatement();
+				String sql = "select * from etudiant";
+				
+				//etape 4 executer la requette
+				System.out.println("Liste des etudiants ");
+				rs = st.executeQuery(sql);		
+				
+				//etape5 parcours du resultSet
+				while (rs.next()) {
+					System.out.print(rs.getString("nom"));
+					System.out.print(" ");
+					System.out.println(rs.getString("prenom"));
+				}
+			
+			} catch (SQLException e) {
+				
+				e.printStackTrace();
+			}
+			catch (ClassNotFoundException e) {
+				
+				e.printStackTrace();
+			}
+			finally {
+				
+				// etape 5 liberer les ressources
+				try {
+					cn.close();
+					st.close();
+				} catch (SQLException e) {
+					
+					e.printStackTrace();
+				}
+			}
+			
+}
 
 }
