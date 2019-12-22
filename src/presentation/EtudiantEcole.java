f29bd9d07e98bc286e4014f66e7d76d4149f320c
zeushias <zeushias89@gmail.com>
2019-12-22 03:32:42 +0100

    Dernier push

--- src/presentation/EtudiantEcole.java
+++ src/presentation/EtudiantEcole.java
@@ -2,348 +2,426 @@ package presentation;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.Scanner;
 
+import metier.Ecole;
 import metier.Etudiant;
 
 public class EtudiantEcole {
-	
-	
 
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
+	public static String password = "";
+
+	public static void creerEcole(Ecole ecole) {
+
 		// information de la base de donnee
-		System.out.println("Pour insérer une école tapez 1");
-		System.out.println("Pour insérer un étudiant tapez 2");
-		System.out.println("Pour lister les écoles tapez 3");
-		System.out.println("Pour lister les étudiants tapez 4");
-		System.out.println("Pour supprimer une école tapez 5");
-		System.out.println("Pour supprimer un étudiant tapez 6");
-		System.out.println("Pour modifier le nom d'une école tapez 7");
-		System.out.println("Pour modifier le nom d'un étudiant tapez 8");
-		
-		System.out.print("Saisir un chiffre");
-		int saisie = scan.nextInt();
-		
-		switch (saisie) {
-		case 1:
-			// enrégistrer une école
-			
-			break;
-		case 2:
-			// enrégistrer un étudiant
-			System.out.print("entrer le nom de l'etudiant : ");
-			String nom = scan.next();
-			System.out.println();
-			System.out.print("entrer le prenom de l'etudiant : ");
-			String prenom = scan.next();
-			Etudiant etudiant = new Etudiant( nom, prenom);
-			creerEtudiant(etudiant);
-			break;
-		case 3:
-			
-			break;
-		case 4:
-			
-			break;
-		case 7:
-			System.out.print("entrer le nom de l'école à modifier: ");
-			String ancienNom = scan.next();
-			System.out.println();
-			System.out.print("entrer le nouveau nom de l'école : ");
-			String nouveauNom = scan.next();
-			updateEtudiant(ancienNom, nouveauNom);
-			break;
-		case 8:
-			System.out.print("entrer le nom de l'etudiant à modifier: ");
-			String ancienNomEcole = scan.next();
-			System.out.println();
-			System.out.print("entrer le nouveau nom de l'etudiant : ");
-			String nouveauNomEcole = scan.next();
-			updateEtudiant(ancienNomEcole, nouveauNomEcole);
-			break;
 
-		default:
-			break;
-		}
-		// TODO Auto-generated method stub
-		
-		
-		
-
-	}
-	
-	
-	
-public static void creerEtudiant(Etudiant etudiant) {
-						
 		String url = "jdbc:mysql://localhost/etudiantecole";
 		String login = "root";
-		String password = "";
+
 		Connection cn = null;
 		Statement st = null;
-		
+
 		try {
 			// etape1 chargement du driver
-			
+
 			Class.forName("com.mysql.jdbc.Driver");
-			
+
 			// etape2 recupertion de la connnexion
-			
+
 			cn = DriverManager.getConnection(url, login, password);
-			
+
 			// etape 3 creation du statement
 			st = cn.createStatement();
-			String sql = "INSERT INTO `etudiant` (`nom`,`prenom`) VALUES ('"+etudiant.getNomEtudiant()+"','"+etudiant.getPrenomEtudiant()+"')";
-					
-			//etape 4 executer la requette
+			String sql = "INSERT INTO `ecole` (`nomEcole`,`adresseEcole`) VALUES ('" + ecole.getNomEcole() + "','"
+					+ ecole.getAdresseEcole() + "')";
+
+			// etape 4 executer la requette
 			st.executeUpdate(sql);
 			System.out.println("Enrégistrement effectué avec succès !!!! ");
-		
+
 		} catch (SQLException e) {
-			
+
 			e.printStackTrace();
-		}
-		catch (ClassNotFoundException e) {
-			
+		} catch (ClassNotFoundException e) {
+
 			e.printStackTrace();
-		}
-		finally {
-			
+		} finally {
+
 			// etape 5 liberer les ressources
 			try {
 				cn.close();
 				st.close();
 			} catch (SQLException e) {
-				
+
 				e.printStackTrace();
 			}
 		}
 	}
-	
 
+	public static void lireEcole() {
 
+		// information de la base de donnee
 
+		String url = "jdbc:mysql://localhost/etudiantecole";
+		String login = "root";
 
+		Connection cn = null;
+		Statement st = null;
+		ResultSet rs = null;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// update de étudiant
-public static void updateEtudiant(String ancienNom, String nouveauNom) {
-	// information de la base de donnee
-
-	String url = "jdbc:mysql://localhost/etudiantecole";
-	String login = "root";
-	String password = "";
-	Connection cn = null;
-	Statement st = null;
-
-	try {
-		// etape1 chargement du driver
-
-		Class.forName("com.mysql.jdbc.Driver");
-
-		// etape2 recupertion de la connnexion
-
-		cn = DriverManager.getConnection(url, login, password);
-
-		// etape 3 creation du statement
-		st = cn.createStatement();
-		String sql = "update etudiant set nom = '" + nouveauNom + "' where nom = '" + ancienNom+"'";
-
-		// etape 4 executer la requette
-		
-		int rs = st.executeUpdate(sql);
-		System.out.println("modification du nom de l'étudiant éffectuée avec succès");
-		
-
-	} catch (SQLException e) {
-		System.out.println("Le nom n'existe pas");
-		e.printStackTrace();
-	} catch (ClassNotFoundException e) {
-
-		e.printStackTrace();
-	} finally {
-
-		// etape 5 liberer les ressources
 		try {
-			cn.close();
-			st.close();
+			// etape1 chargement du driver
+
+			Class.forName("com.mysql.jdbc.Driver");
+
+			// etape2 recupertion de la connnexion
+
+			cn = DriverManager.getConnection(url, login, password);
+
+			// etape 3 creation du statement
+			st = cn.createStatement();
+			String sql = "select * from ecole";
+
+			// etape 4 executer la requette
+			System.out.println("Liste des ecoles ");
+			rs = st.executeQuery(sql);
+
+			// etape5 parcours du resultSet
+			while (rs.next()) {
+				System.out.print(rs.getString("nomEcole"));
+				System.out.print(" ");
+				System.out.println(rs.getString("adresseEcole"));
+			}
+
 		} catch (SQLException e) {
 
 			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+
+			e.printStackTrace();
+		} finally {
+
+			// etape 5 liberer les ressources
+			try {
+				cn.close();
+				st.close();
+			} catch (SQLException e) {
+
+				e.printStackTrace();
+			}
 		}
+
 	}
 
-}
-// update école
-public static void updateEcole(String ancienNom, String nouveauNom) {
-	// information de la base de donnee
+	public static void creerEtudiant(Etudiant etudiant, String nomEcole) {
 
-	String url = "jdbc:mysql://localhost/etudiantecole";
-	String login = "root";
-	String password = "";
-	Connection cn = null;
-	Statement st = null;
+		// information de la base de donnee
 
-	try {
-		// etape1 chargement du driver
+		String url = "jdbc:mysql://localhost/etudiantecole";
+		String login = "root";
 
-		Class.forName("com.mysql.jdbc.Driver");
+		Connection cn = null;
+		Statement st = null;
+		ResultSet rs = null;
 
-		// etape2 recupertion de la connnexion
-
-		cn = DriverManager.getConnection(url, login, password);
-
-		// etape 3 creation du statement
-		st = cn.createStatement();
-		String sql = "update ecole set nomEcole = '" + nouveauNom + "' where nomEcole = '" + ancienNom+"'";
-
-		// etape 4 executer la requette
-		
-		int rs = st.executeUpdate(sql);
-		System.out.println("modification du nom de l'école éffectuée avec succès");
-		
-
-	} catch (SQLException e) {
-		System.out.println("Le nom n'existe pas");
-		e.printStackTrace();
-	} catch (ClassNotFoundException e) {
-
-		e.printStackTrace();
-	} finally {
-
-		// etape 5 liberer les ressources
 		try {
-			cn.close();
-			st.close();
+			// etape1 chargement du driver
+
+			Class.forName("com.mysql.jdbc.Driver");
+
+			// etape2 recupertion de la connnexion
+
+			cn = DriverManager.getConnection(url, login, password);
+
+			// etape 3 creation du statement
+			st = cn.createStatement();
+			String sql = "select idEcole from ecole where nomEcole='" + nomEcole + "'";
+
+			rs = st.executeQuery(sql);
+			int idEcole = 0;
+			while (rs.next()) {
+				idEcole = rs.getInt(1);
+
+			}
+
+			String sql1 = "INSERT INTO `etudiant` (`nom`,`prenom`,`idEcole`) VALUES ('" + etudiant.getNomEtudiant()
+					+ "','" + etudiant.getPrenomEtudiant() + "','" + idEcole + "')";
+
+			// etape 4 executer la requette
+			st.executeUpdate(sql1);
+			System.out.println("Enrégistrement effectué avec succès !!!! ");
+
 		} catch (SQLException e) {
 
 			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+
+			e.printStackTrace();
+		} finally {
+
+			// etape 5 liberer les ressources
+			try {
+				cn.close();
+				st.close();
+			} catch (SQLException e) {
+
+				e.printStackTrace();
+			}
 		}
 	}
 
-}
-// delete étudiant
+	public static void lireEtudiant() {
 
+		// information de la base de donnee
 
+		String url = "jdbc:mysql://localhost/etudiantecole";
+		String login = "root";
 
+		Connection cn = null;
+		Statement st = null;
+		ResultSet rs = null;
 
+		try {
+			// etape1 chargement du driver
 
+			Class.forName("com.mysql.jdbc.Driver");
+
+			// etape2 recupertion de la connnexion
+
+			cn = DriverManager.getConnection(url, login, password);
+
+			// etape 3 creation du statement
+			st = cn.createStatement();
+			String sql = "select * from etudiant";
+
+			// etape 4 executer la requette
+			System.out.println("Liste des etudiants ");
+			rs = st.executeQuery(sql);
+
+			// etape5 parcours du resultSet
+			while (rs.next()) {
+				System.out.println(rs.getInt("idEcole"));
+				System.out.println(" ");
+				System.out.print(rs.getString("nom"));
+				System.out.print(" ");
+				System.out.println(rs.getString("prenom"));
+
+			}
+
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+
+			e.printStackTrace();
+		} finally {
+
+			// etape 5 liberer les ressources
+			try {
+				cn.close();
+				st.close();
+			} catch (SQLException e) {
+
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public static void deleteEtudiant(String nom) {
+
+		// information de la base de donnee
+
+		String url = "jdbc:mysql://localhost/etudiantecole";
+		String login = "root";
+
+		Connection cn = null;
+		Statement st = null;
+		ResultSet rs = null;
+
+		try {
+			// etape1 chargement du driver
+
+			Class.forName("com.mysql.jdbc.Driver");
+
+			// etape2 recupertion de la connnexion
+
+			cn = DriverManager.getConnection(url, login, password);
+
+			// etape 3 creation du statement
+			st = cn.createStatement();
+			String sql = "delete from `etudiant` where `nom` = '" + nom + "'";
+			// etape 4 executer la requette
+			st.executeUpdate(sql);
+
+			// etape5 parcours du resultSet
+			System.out.println("Suppression éffectuée avec succès");
+
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+
+			e.printStackTrace();
+		} finally {
+
+			// etape 5 liberer les ressources
+			try {
+				cn.close();
+				st.close();
+			} catch (SQLException e) {
+
+				e.printStackTrace();
+			}
+		}
+	}
+
+//update de étudiant
+	public static void updateEtudiant(String ancienNom, String nouveauNom) {
+		// information de la base de donnee
+
+		String url = "jdbc:mysql://localhost/etudiantecole";
+		String login = "root";
+
+		Connection cn = null;
+		Statement st = null;
+
+		try {
+			// etape1 chargement du driver
+
+			Class.forName("com.mysql.jdbc.Driver");
+
+			// etape2 recupertion de la connnexion
+
+			cn = DriverManager.getConnection(url, login, password);
+
+			// etape 3 creation du statement
+			st = cn.createStatement();
+			String sql = "update etudiant set nom = '" + nouveauNom + "' where nom = '" + ancienNom + "'";
+
+			// etape 4 executer la requette
+
+			int rs = st.executeUpdate(sql);
+			System.out.println("modification du nom de l'étudiant éffectuée avec succès");
+
+		} catch (SQLException e) {
+			System.out.println("Le nom n'existe pas");
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+
+			e.printStackTrace();
+		} finally {
+
+			// etape 5 liberer les ressources
+			try {
+				cn.close();
+				st.close();
+			} catch (SQLException e) {
+
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+//update école
+	public static void updateEcole(String ancienNom, String nouveauNom) {
+		// information de la base de donnee
+
+		String url = "jdbc:mysql://localhost/etudiantecole";
+		String login = "root";
+		Connection cn = null;
+		Statement st = null;
+
+		try {
+			// etape1 chargement du driver
+
+			Class.forName("com.mysql.jdbc.Driver");
+
+			// etape2 recupertion de la connnexion
+
+			cn = DriverManager.getConnection(url, login, password);
+
+			// etape 3 creation du statement
+			st = cn.createStatement();
+			String sql = "update ecole set nomEcole = '" + nouveauNom + "' where nomEcole = '" + ancienNom + "'";
+
+			// etape 4 executer la requette
+
+			int rs = st.executeUpdate(sql);
+			System.out.println("modification du nom de l'école éffectuée avec succès");
+
+		} catch (SQLException e) {
+			System.out.println("Le nom n'existe pas");
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+
+			e.printStackTrace();
+		} finally {
+
+			// etape 5 liberer les ressources
+			try {
+				cn.close();
+				st.close();
+			} catch (SQLException e) {
+
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public static void deleteEcole(String nom) {
+
+		// information de la base de donnee
+
+		String url = "jdbc:mysql://localhost/etudiantecole";
+		String login = "root";
+		Connection cn = null;
+		Statement st = null;
+		ResultSet rs = null, rs2 = null;
+
+		try {
+			// etape1 chargement du driver
+
+			Class.forName("com.mysql.jdbc.Driver");
+
+			// etape2 recupertion de la connnexion
+
+			cn = DriverManager.getConnection(url, login, password);
+
+			// etape 3 creation du statement
+			st = cn.createStatement();
+			String sqlVerification = "SELECT distinct `idEcole` FROM `etudiant` WHERE `idEcole` = (SELECT `idEcole` FROM `ecole` WHERE `nomEcole` = '"
+					+ nom + "')";
+			rs2 = st.executeQuery(sqlVerification);
+			if (rs2.next()) {
+				System.out.println("Suppression impossible. L'école est reliée à des étudiants");
+			} else {
+
+				String sql = "delete from `ecole` where `nomEcole` = '" + nom + "'";
+				// etape 4 executer la requette
+				st.executeUpdate(sql);
+
+				// etape5 parcours du resultSet
+				System.out.println("Suppression éffectuée avec succès");
+			}
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+
+			e.printStackTrace();
+		} finally {
+
+			// etape 5 liberer les ressources
+			try {
+				cn.close();
+				st.close();
+			} catch (SQLException e) {
+
+				e.printStackTrace();
+			}
+		}
+	}
 }
--- src/presentation/luncher.java
+++ src/presentation/luncher.java
@@ -1,9 +1,200 @@
 package presentation;
 
+import java.util.Scanner;
+
+import javax.management.openmbean.OpenMBeanOperationInfo;
+
+import metier.Ecole;
+import metier.Etudiant;
+
 public class luncher {
 
+	private static void afficher() {
+		Scanner scan = new Scanner(System.in);
+
+		// affichage basique
+		System.out.println("--------------------------------------------------------------");
+		System.out.println("Pour insérer une école tapez 1");
+		System.out.println("Pour insérer un étudiant tapez 2");
+		System.out.println("Pour lister les écoles tapez 3");
+		System.out.println("Pour lister les étudiants tapez 4");
+		System.out.println("Pour supprimer une école tapez 5");
+		System.out.println("Pour supprimer un étudiant tapez 6");
+		System.out.println("Pour modifier le nom d'une école tapez 7");
+		System.out.println("Pour modifier le nom d'un étudiant tapez 8");
+		System.out.println("--------------------------------------------------------------");
+		//
+		System.out.print("Saisir un chiffre : ");
+
+	}
+
+	private static Integer saisieOperation() {
+		// saisir une numéro qui correspond à une opération
+		Scanner scan = new Scanner(System.in);
+		int saisie = scan.nextInt();
+		return saisie;
+	}
+
+	// regroupement des méthodes qui se répètent
+	private static void methodeARepetition() {
+		// appel de la méthode afficher
+		afficher();
+		// récupération du chiffre saisie
+		int saisie = saisieOperation();
+		// opération à exécuter
+		operation(saisie);
+	}
+
+	/*
+	 * mot de passe base de donnée
+	 */
+	private static void saisirMotDePasse() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("--------------------------------------------------------------");
+		System.out.print("Avez vous un mot de passe pour mysql O / N : ");
+		String reponse = sc.next().toUpperCase();
+		if (reponse.equals("O")) {
+			System.out.println();
+			System.out.print("Veuillez saisir le mot de passe de votre base mysql : ");
+			System.out.println();
+			EtudiantEcole.password = sc.next();
+		}
+	}
+
+	// méthode main
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// mot de passe mysql
+		saisirMotDePasse();
+		// opération à faire
+		methodeARepetition();
+
+	}
+
+	/*
+	 * demander si l'utilisateur veut poursuivre les opérations
+	 */
+	private static void continuerOperation() {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Voulez-vous continuer? O / N : ");
+		System.out.println();
+		String reponse = sc.next().toUpperCase();
+		//
+		switch (reponse) {
+
+		case "O":
+			// recommencer
+			methodeARepetition();
+			break;
+		case "N":
+			// finir le programme
+			System.out.println("Au revoir !!!");
+			break;
+		default:
+			// reposer la question
+			System.out.println("Mauvaise reponse");
+			continuerOperation();
+			break;
+		}
+	}
+
+	/*
+	 * opération à exécuter
+	 */
+	private static void operation(int chiffre) {
+		Scanner scan = new Scanner(System.in);
+		switch (chiffre) {
+		case 1:
+			// enrégistrer une école
+			System.out.println();
+			System.out.print("entrer le nom de l'école : ");
+			String nomEcol = scan.next();
+			System.out.println();
+			System.out.print("entrer l'adresse de l'école : ");
+			String adresse = scan.next();
+			Ecole ecole = new Ecole(nomEcol, adresse);
+			EtudiantEcole.creerEcole(ecole);
+			continuerOperation();
+			break;
+
+		case 2:
+			System.out.println();
+			// enrégistrer un étudiant
+			System.out.print("entrer le nom de l'etudiant : ");
+			String nom = scan.next();
+			System.out.println();
+			System.out.print("entrer le prenom de l'etudiant : ");
+			String prenom = scan.next();
+			System.out.println();
+			System.out.print("entrer le nom de l'école : ");
+			String nomEcole = scan.next();
+			Etudiant etudiant = new Etudiant(nom, prenom);
+			EtudiantEcole.creerEtudiant(etudiant, nomEcole);
+			continuerOperation();
+			break;
+
+		case 3:
+			// lister les écoles
+			System.out.println();
+			System.out.println("La liste des écoles");
+			EtudiantEcole.lireEcole();
+			continuerOperation();
+			break;
+		case 4:
+			// lister les étudiants
+			System.out.println();
+			System.out.println("La liste des étudiants");
+			EtudiantEcole.lireEtudiant();
+			continuerOperation();
+			break;
+		case 5:
+			// supprimer une école
+			System.out.println();
+			System.out.print("entrer le nom de l'école à supprimer: ");
+			String nomEcoleSupprimer = scan.next();
+			EtudiantEcole.deleteEcole(nomEcoleSupprimer);
+			continuerOperation();
+			break;
+
+		case 6:
+			// supprimer un étudiant
+			System.out.println();
+			System.out.print("entrer le nom de l'étudiant à supprimer: ");
+			String nomEtudiantSupprimer = scan.next();
+			EtudiantEcole.deleteEtudiant(nomEtudiantSupprimer);
+			continuerOperation();
+			break;
+
+		case 7:
+			// modifer une école
+			System.out.println();
+			System.out.print("entrer le nom de l'école à modifier: ");
+			String ancienNomEcole = scan.next();
+			System.out.println();
+			System.out.print("entrer le nouveau nom de l'école : ");
+			String nouveauNomEcole = scan.next();
+			EtudiantEcole.updateEcole(ancienNomEcole, nouveauNomEcole);
+			continuerOperation();
+			break;
+		case 8:
+			// modifier un étudiant
+			System.out.println();
+			System.out.print("entrer le nom de l'etudiant à modifier: ");
+			String ancienNom = scan.next();
+			System.out.println();
+			System.out.print("entrer le nouveau nom de l'etudiant : ");
+			String nouveauNom = scan.next();
+			EtudiantEcole.updateEtudiant(ancienNom, nouveauNom);
+			continuerOperation();
+			break;
+
+		default:
+			// si aucune opération ne correspond
+			System.out.println();
+			System.out.println("Vous avez saisi un mauvais chiffre. Veuillez reprendre Merci.");
+			methodeARepetition();
+			break;
+		}
 
 	}
 
